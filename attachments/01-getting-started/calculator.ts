12d3c0dfe8bf80b1705636fd353a81d066e39679
Jiao,Jian <jjiao@ab.bluecross.ca>
2025-04-11 14:41:40 -0600

    add .ts file to calculator demo

--- attachments/01-getting-started/calculator.ts
+++ attachments/01-getting-started/calculator.ts
@@ -1,4 +1,4 @@
-function deriveFinalPrice(inputPrice) {
+function deriveFinalPrice(inputPrice: number) {
   const finalPrice = inputPrice + inputPrice * 0.19;
   const outputEl = document.getElementById("final-price");
   outputEl.textContent = "Final Price: " + finalPrice + " €";
@@ -8,7 +8,7 @@ const formEl = document.querySelector("form");
 
 formEl.addEventListener("submit", function (event) {
   event.preventDefault();
-  const fd = new FormData(event.currentTarget);
+  const fd = new FormData(event.currentTarget as HTMLFormElement);
   const inputPrice = fd.get("price");
   deriveFinalPrice(inputPrice);
 });
