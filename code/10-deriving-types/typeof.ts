f8a4f72a8dff0f6a384c2746b6849d3d7289f5a5
Jiao Jian <jj.jianjiao@gmail.com>
2025-05-07 09:14:40 -0600

    typeof another case

--- code/10-deriving-types/typeof.ts
+++ code/10-deriving-types/typeof.ts
@@ -1,14 +1,14 @@
-let userName = 'Max';
+let userName = "Max";
 
 console.log(typeof userName);
 
 type UserName = typeof userName;
 
 const settings = {
-  difficulty: 'easy',
+  difficulty: "easy",
   minLevel: 10,
   didStart: false,
-  players: ['John', 'Jane']
+  players: ["John", "Jane"],
 };
 
 // type Settings = typeof settings;
@@ -18,3 +18,21 @@ function loadData(s: typeof settings) {
 }
 
 loadData(settings);
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+function subtract(a: number, b: number) {
+  return a - b;
+}
+
+type SumFn = typeof sum;
+type SubtractFn = typeof subtract;
+
+// function performMathAction(cb: (a: number, b: number) => number) {
+//   // some code...
+// }
+
+function performMathAction(cb: SumFn | SubtractFn) {
+  // some code...
+}
